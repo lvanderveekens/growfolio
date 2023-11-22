c5d8f63bbbbe3e1ec14b045b95faee646f5c21f6
Luciano van der Veekens <lvanderveekens@proton.me>
2023-11-22 20:24:23 +0100

    fix

--- frontend/app/modal.tsx
+++ frontend/app/modal.tsx
@@ -38,8 +38,8 @@ const Modal: React.FC<ModalProps> = ({ title, onClose, children }) => {
   return (
     <div className={`z-50 fixed inset-0 flex items-center justify-center`}>
       <div ref={modalBackgroundRef} className="fixed inset-0 bg-black opacity-50"></div>
-      <div className="bg-white rounded-md m-4 p-4 lg:m-8 lg:p-8 z-10 w-full lg:w-auto">
-        <div className="mb-4 flex justify-between items-center w-full lg:w-[400px] ">
+      <div className="bg-white rounded-md m-4 p-4 lg:m-8 lg:p-8 z-10 box-content w-full lg:w-[400px]">
+        <div className="mb-4 flex justify-between items-center w-full">
           <span className="text-lg font-bold">{title}</span>
           <FaXmark
             className="hover:cursor-pointer"
