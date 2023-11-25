9be95fce99b8a856930cd09f3a97f36e91785c6e
Luciano van der Veekens <lvanderveekens@proton.me>
2023-11-25 17:34:37 +0100

    Attempt at implementing scrollable modals

--- frontend/app/modal.tsx
+++ frontend/app/modal.tsx
@@ -37,17 +37,27 @@ const Modal: React.FC<ModalProps> = ({ title, onClose, children }) => {
 
   return (
     <div className={`z-50 fixed inset-0 flex items-center justify-center`}>
-      <div ref={modalBackgroundRef} className="fixed inset-0 bg-black opacity-50"></div>
-      <div className="bg-white rounded-md m-4 p-4 lg:m-8 lg:p-8 z-10 box-content w-full lg:w-[400px]">
-        <div className="mb-4 flex justify-between items-center w-full">
-          <span className="text-lg font-bold">{title}</span>
-          <FaXmark
-            className="hover:cursor-pointer"
-            size={24}
-            onClick={onClose}
-          />
+      <div
+        ref={modalBackgroundRef}
+        className="fixed inset-0 bg-black opacity-50"
+      ></div>
+      <div className="flex z-10 max-h-screen">
+        <div className="flex flex-col bg-white w-full lg:w-[400px] box-content  rounded-md m-4 p-4 lg:m-8 lg:p-8">
+
+          {/* header */}
+          <div className="mb-4 flex justify-between items-center w-full">
+            <span className="text-lg font-bold">{title}</span>
+            <FaXmark
+              className="hover:cursor-pointer"
+              size={24}
+              onClick={onClose}
+            />
+          </div>
+          
+          {/* content */}
+          <div className="overflow-y-auto">{children}</div>
+
         </div>
-        {children}
       </div>
     </div>
   );
